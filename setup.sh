e7ec18a7692e5c29b4d2183a759b56e3e7eaadbc
gyaneman <gyanexus7@gmail.com>
2017-10-02 23:58:54 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 echo "===== setup ====="
 
-ln -sf $PWD/resources/.vimrc $HOME/.vimrc
-ln -sf $PWD/resources/.gitconfig $HOME/.gitconfig
+ln -sf $PWD/resources/vimrc $HOME/.vimrc
+ln -sf $PWD/resources/gitconfig $HOME/.gitconfig
 
 mkdir -p $HOME/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
