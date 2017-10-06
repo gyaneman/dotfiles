3e2303760bce1002aa971489b3f1a4ee80b2b2c4
gyaneman <gyanexus7@gmail.com>
2017-10-07 04:52:05 +0900

    add .zshrc and update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "===== setup ====="
-
 ln -sf $PWD/resources/vimrc $HOME/.vimrc
+ln -sf $PWD/resources/zshrc $HOME/.zshrc
 ln -sf $PWD/resources/gitconfig $HOME/.gitconfig
 
 mkdir -p $HOME/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+
+if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ] ; then
+  git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+fi
