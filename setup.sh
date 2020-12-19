b8943d109ccd5d2a56983f331695e335b534f477
gyaneman <taka4456950@gmail.com>
2020-12-19 15:35:26 +0900

    install nvim

--- setup.sh
+++ setup.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-ln -sf $PWD/resources/vimrc $HOME/.vimrc
+mkdir -p $HOME/.config/nvim/
+ln -sf $PWD/resources/init.vim $HOME/.config/nvim/init.vim
 ln -sf $PWD/resources/zshrc $HOME/.zshrc
 #ln -sf $PWD/resources/gitconfig $HOME/.gitconfig
 
-mkdir -p $HOME/.vim/bundle
-
-if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ] ; then
-  git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
-fi
-
-
-ln -sf $PWD/resources/vim/ftplugin/ $HOME/.vim/ftplugin
